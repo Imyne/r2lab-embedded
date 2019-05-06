75fd31fe514ee67a64fc5514b5f799633a6afcab
imaneouss <ioussake@laas.fr>
2019-05-06 13:15:16 +0200

    message de commit

--- shell/oai-gw.sh
+++ shell/oai-gw.sh
@@ -523,7 +523,7 @@ function start-epc() {
     if [ -n "$runs_epc" ]; then
 	cd $run_dir
 	echo "Launching mme and spgw in background"
-	./run_mme >& $log_mme &
+	./run_mme 2>&1 & $log_mme &
 #	./run_spgw -r >& $log_spgw &
 	./run_spgw >& $log_spgw &
     fi
