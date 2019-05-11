eaa231c2f5318123cf10058ebea53fdbcb011bec
imaneouss <ioussake@laas.fr>
2019-05-11 19:30:27 +0200

    message de commit

--- shell/iperf.sh
+++ shell/iperf.sh
@@ -4,15 +4,16 @@ apt update
 
 apt install iperf3 -y
 
-
 ifup enx0c5b8f279a64
 
 
 address_dest=$1
 interface_to_bind="enx0c5b8f279a64"
-port=$2
+bandwidth=$2
+port=$3
+time=$4
 
-iperf3 -c $1 -B $interface_to_bind -u -p $2
+iperf3 -c $1 -B $interface_to_bind -u -b $2 -p $3 -t $4
 
 
 
