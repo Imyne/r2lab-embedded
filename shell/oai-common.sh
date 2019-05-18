3c924e5014da27dbdd155ce2f79ca85948b764ee
imaneouss <ioussake@laas.fr>
2019-05-18 16:31:47 +0200

    message de commit

--- shell/oai-common.sh
+++ shell/oai-common.sh
@@ -16,7 +16,7 @@ oai_subnet=2
 ####################
 doc-nodes git-pull-oai "updates OAI repos /root/openair-cn /root/openairinterface5g from git"
 #function git-pull-oai() { -git-pull-repos /root/openair-cn@develop /root/openairinterface5g@develop; }
-function git-pull-oai() { -git-pull-repos /root/openair-cn@develop /root/openairinterface5g; } #openairinterface5g@2017.w34
+function git-pull-oai() { -git-pull-repos /root/openair-cn@develop /root/openairinterface5g@2017.w34; } #openairinterface5g@2017.w34
 
 #function git-pull-oai() { git pull https://gitlab.eurecom.fr/imaneouss/openairinterface5g.git; }
 
