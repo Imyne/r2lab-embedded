34a90694de51768590527c56bc77f13c7dbe2dc9
imaneouss <ioussake@laas.fr>
2019-05-27 17:20:45 +0200

    message de commit

--- shell/tracer-start.sh
+++ shell/tracer-start.sh
@@ -14,8 +14,7 @@ function start-tracer() {
     cd ..
     cd /root/openairinterface5g/common/utils/T/tracer
     make
-    ./textlog -no-gui -ON -d ../T_messages.txt -ip 192.168.3.23 >& tracelog.log
-    #./textlog -no-gui -on ENB_PHY_DLSCH_UE_DCI -on ENB_PHY_ULSCH_UE_DCI -d ../T_messages.txt -ip 192.168.3.23 >& tracelog.log
+    ./textlog -no-gui -on ENB_PHY_ULSCH_UE_DCI -on ENB_PHY_ULSCH_UE_NO_DCI_RETRANSMISSION -on ENB_PHY_UL_CHANNEL_ESTIMATE -on ENB_PHY_PUSCH_IQ -on ENB_PHY_PUCCH_1_ENERGY -on ENB_PHY_PHICH -on ENB_MAC_UE_UL_SCHEDULE -on ENB_MAC_UE_UL_PDU -on ENB_RLC_UL -on LEGACY_UDP__INFO -on LEGACY_RLC_INFO -on LEGACY_MAC_INFO -on LEGACY_MAC_DEBUG -on LEGACY_PHY_INFO -on LEGACY_PHY_DEBUG -on UE_PHY_MEAS -d ../T_messages.txt -ip 192.168.3.23 >& tracelog.log
 
 }
 
