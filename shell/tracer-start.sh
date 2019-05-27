c7459e0e1613bfd5c8cd2247bf709333e1010b2b
imaneouss <ioussake@laas.fr>
2019-05-27 16:48:57 +0200

    message de commit

--- shell/tracer-start.sh
+++ shell/tracer-start.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+
+[ -z "$_sourced_nodes" ] && source $(dirname $(readlink -f $BASH_SOURCE))/nodes.sh
+
+doc-nodes-sep "#################### For managing an OAI enodeb"
+
+[ -z "$_sourced_oai_common" ] && source $(dirname $(readlink -f $BASH_SOURCE))/oai-common.sh
+
+
+doc-nodes start-tracer "start tracer on node 19 yooliiiii"
 function start-tracer() {
     echo "starting the T tracer"
-    cd [ -d /root/openairinterface5g/common/utils/T/tracer]
-    run-in-log build-tracer.log ./textlog -no-gui -ON -d ../T_messages.txt -ip 192.168.3.23
+    cd ..
+    cd /root/openairinterface5g/common/utils/T/tracer
+    make
+    ./textlog -no-gui -on ENB_PHY_DLSCH_UE_DCI -on ENB_PHY_ULSCH_UE_DCI -d ../T_messages.txt -ip 192.168.3.23 >& tracelog.log
+
 }
+
+
+########################################
+define-main "$0" "$BASH_SOURCE"
+main "$@"
