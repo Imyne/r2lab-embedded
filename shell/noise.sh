d108eb48a77094f9eeb573fb9c9a6a5d3f05feca
root <root@fit11.lan>
2019-05-28 14:07:49 +0200

    message de commit

--- shell/noise.sh
+++ shell/noise.sh
@@ -3,14 +3,14 @@
 downlink_freq="--freq=2.66G"
 uplink_freq="--freq=2.54G"
 
-gains=($(shuf -i 10-90 -n 15))
+gains=($(shuf -i 20-90 -n 15))
 echo "${gains[@]}"
 
+command="uhd_siggen --gaussian"
 for force in "${gains[@]}"; do
    #ampl= $(( (RANDOM % 10) +1 ))
-   command="uhd_siggen --gaussian $uplink_freq -m $(( (RANDOM % 15) +1 ))"
-
-   command="$command -g $force"
+   command="$command $uplink_freq -m $(( (RANDOM % 15) +1 )) -g $force"
+  # command="$command -g $force"
    echo "About to run command:"
    echo $command
    #$command & pidsave=$!
