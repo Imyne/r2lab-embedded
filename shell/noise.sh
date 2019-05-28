8c497993c5addab41baa070dbc0ca19b2dd7a762
imaneouss <ioussake@laas.fr>
2019-05-28 13:08:02 +0200

    message de commit

--- shell/noise.sh
+++ shell/noise.sh
@@ -4,39 +4,17 @@ downlink_freq="--freq=2.66G"
 uplink_freq="--freq=2.54G"
 
 
-'''function scrambling-noise{ 
 
-    local command="uhd_siggen --gaussian $uplink_freq"
-  
-    for force in 70 100; do
-	
-	command="$command -g $force"
-    	echo "About to run command:"
-    	echo $command
-    	$command
-	sleep 60
-   done
-}'''
-
-command="uhd_siggen --gaussian $uplink_freq"
-  
-for force in 70 100; do
+for force in 70 100 80; do
+   ampl= $(( (RANDOM % 10) +1 ))
+   command="uhd_siggen --gaussian $uplink_freq -m $ampl"
    command="$command -g $force"
    echo "About to run command:"
    echo $command
    #$command & pidsave=$!
    #wait 30;kill $pidsave
   #timeout 30s $command
-   timelimit -t30 $command
+   timelimit -t10 $command
 done
-
-'''command="uhd_siggen --gaussian $uplink_freq"
-force=70
-while $force<; do
-	force=$(($force+10))
-	echo $force
-	command="$command -g $force"
-        echo "About to run command:"
-   	echo $command
-   	$command'''	
+	
 
